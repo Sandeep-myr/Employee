9e92b7275adf0189c48f4e61f269a33fd8904164
sandeep sahu <sandeepsahu@rajutech.com>
2024-03-06 18:23:14 +0530

    name

--- src/main/java/com/emp/employee/MyRunner.java
+++ src/main/java/com/emp/employee/MyRunner.java
@@ -17,7 +17,7 @@ public class MyRunner implements  CommandLineRunner	 {
 		
 		Employee emp1 = new Employee(111,"sandeep","developer","7000517223","sansahu921@gmail.com",22,12000);
 		Employee emp2 = new Employee(112,"satyendra","Manager","7000552875","satyendra21@gmail.com",25,19000);
-		Employee emp3 = new Employee(113,"Manish","Tester","7693937561","Manish21@gmail.com",19,42000);
+		Employee emp3 = new Employee(113,"Manish Sahu 123 ","Tester","7693937561","Manish21@gmail.com",19,42000);
 		Employee emp4 = new Employee(114,"Pankaj","Manager","9685719998","pankaj7725@gmail.com",29,20000);
 		Employee emp5= new Employee(115,"Akhil","developer","7000531275","akhil@gmail.com",50,2000);
 		
